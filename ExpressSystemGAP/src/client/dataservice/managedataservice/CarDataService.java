e30ff762f94da81a98cc90a3bcd07029e1d391c2
Christine-Seven <txy24601@126.com>
2015-10-24 00:18:55 +0800

    txy

--- ExpressSystemGAP/src/client/dataservice/managedataservice/CarDataService.java
+++ ExpressSystemGAP/src/client/dataservice/managedataservice/CarDataService.java
@@ -1,6 +1,11 @@
 package client.dataservice.managedataservice;
 
+import client.po.CarPO;
+import client.po.ResultMessage;
 
 public interface CarDataService {
-
+    public ResultMessage  insert(CarPO po);
+    public CarPO find(String user_id);
+    public ResultMessage delete(String user_id);
+    public ResultMessage update(CarPO po);
 }
--- ExpressSystemGAP/src/client/dataservice/userdataservice/UserDataService.java
+++ ExpressSystemGAP/src/client/dataservice/userdataservice/UserDataService.java
@@ -1,5 +1,11 @@
 package client.dataservice.userdataservice;
 
-public interface UserDataService {
+import client.po.ResultMessage;
+import client.po.UserPO;
 
+public interface UserDataService {
+          public ResultMessage  insert(UserPO po);
+          public UserPO find(String user_id);
+          public ResultMessage delete(String user_id);
+          public ResultMessage update(UserPO po);
 }
