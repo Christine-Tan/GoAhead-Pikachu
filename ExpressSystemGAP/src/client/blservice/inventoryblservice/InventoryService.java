341370b6d146300066cb522e5dd61c3d27ec45fc
Panda96 <878924390@qq.com>
2015-10-25 14:17:45 +0800

    inventory

--- ExpressSystemGAP/src/client/blservice/inventoryblservice/InventoryService.java
+++ ExpressSystemGAP/src/client/blservice/inventoryblservice/InventoryService.java
@@ -1,5 +1,35 @@
 package client.blservice.inventoryblservice;
 
-public interface InventoryService {
+import java.util.List;
 
+import po.ExpressOrderPO;
+import po.ResultMessage;
+import client.vo.StockCheckVO;
+import client.vo.StockObVO;
+import client.vo.StockinOrderVO;
+import client.vo.StockoutOrderVO;
+
+public interface InventoryService {
+	public StockObVO observeStock(String begin,String end);
+	
+	public StockCheckVO countStock();
+	
+	public ResultMessage setAlarm (int alarmValue);
+	
+	public ResultMessage distributeSector(String beginColumn,String endColumn,String toSector);
+	
+	public ResultMessage initialadd (ExpressOrderPO expressorder,String location);
+	
+	public ResultMessage initialdelete(String expressorder_id);
+	
+	public ResultMessage initialmodify (ExpressOrderPO expressorder,String location);
+	
+	public String stockIn(ExpressOrderPO expressorder);
+	
+	public StockinOrderVO createStockinOrder(List<ExpressOrderPO> expressorders);
+	
+	public StockoutOrderVO createStockoutOrder(List<ExpressOrderPO> expressorders);
+	
+	public ExpressOrderPO getSingleExpressOrder(String expressorder_id);
+	
 }
--- ExpressSystemGAP/src/dataservice/inventorydataservice/InventoryDataService.java
+++ ExpressSystemGAP/src/dataservice/inventorydataservice/InventoryDataService.java
@@ -1,5 +1,19 @@
 package dataservice.inventorydataservice;
 
+import po.ExpressOrderPO;
+import po.ResultMessage;
+
 public interface InventoryDataService {
+	public ResultMessage add(ExpressOrderPO expressorder);
+	
+	public ResultMessage delete(String expressorder_id);
+	
+	public ResultMessage modify(ExpressOrderPO expressorder,String location);
+	
+	public ExpressOrderPO find(String expressorder_id);
+	
+	public ResultMessage setAlarm(int alarmValue);
+	
+	public int getAlarm();
 
 }
