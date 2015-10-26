b835f5610b784a8e030b8a5b6de22b4d8f06860c
彬 申 <705103197@qq.com>
2015-10-26 22:53:25 +0800

    一些stub

--- ExpressSystemGAP/src/po/ReceiptPO.java
+++ ExpressSystemGAP/src/po/ReceiptPO.java
@@ -1,5 +1,32 @@
 package po;
 
-public class ReceiptPO {
+import java.util.Date;
 
+public class ReceiptPO {
+	
+	private int money;
+	private Date lastPayDate;
+	
+	public ReceiptPO(int money,Date lastPayDate){
+		this.setMoney(money);
+		this.setLastPayDate(lastPayDate);
+		
+	}
+
+	public int getMoney() {
+		return money;
+	}
+
+	public void setMoney(int money) {
+		this.money = money;
+	}
+
+	public Date getLastPayDate() {
+		return lastPayDate;
+	}
+
+	public void setLastPayDate(Date lastPayDate) {
+		this.lastPayDate = lastPayDate;
+	}
+	
 }
---
+++ ExpressSystemGAP/src/stub_drivers/blservice/account/accountbl_stub.java
@@ -0,0 +1,21 @@
+package stub_drivers.blservice.account;
+
+import client.blservice.accountblservice.AccountService;
+import client.vo.AccountListVO;
+import util.ResultMessage;
+
+public class accountbl_stub implements AccountService {
+
+	@Override
+	public AccountListVO getAccountManageList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResultMessage setAccountManageList(AccountListVO accountListVO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
