f5889f6de2561e7dcfe9b03a1dd7932c68a96ba4
彬 申 <705103197@qq.com>
2015-10-26 21:12:51 +0800

    填充PO

--- ExpressSystemGAP/src/po/BillPO.java
+++ ExpressSystemGAP/src/po/BillPO.java
@@ -4,6 +4,34 @@ import com.mysql.fabric.xmlrpc.base.Data;
 
 public class BillPO {
 
-	Data 
+	private Data billDate;
+	private int money;
+	private String courierID;
+	private String expressOrderID;
+	public Data getBillDate() {
+		return billDate;
+	}
+	public void setBillDate(Data billDate) {
+		this.billDate = billDate;
+	}
+	public String getCourierID() {
+		return courierID;
+	}
+	public void setCourierID(String courierID) {
+		this.courierID = courierID;
+	}
+	public int getMoney() {
+		return money;
+	}
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	public String getExpressOrderID() {
+		return expressOrderID;
+	}
+	public void setExpressOrderID(String expressOrderID) {
+		this.expressOrderID = expressOrderID;
+	}
+	
 	
 }
