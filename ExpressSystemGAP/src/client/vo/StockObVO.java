53a1ab79040dd596ee4f2e743a14066b0840683b
Panda96 <878924390@qq.com>
2015-10-26 23:18:55 +0800

    部分VO

--- ExpressSystemGAP/src/client/vo/StockObVO.java
+++ ExpressSystemGAP/src/client/vo/StockObVO.java
@@ -1,5 +1,15 @@
 package client.vo;
 
-public class StockObVO {
+import java.util.List;
 
+public class StockObVO {
+	private List<GoodsVO> list;
+
+	public List<GoodsVO> getList() {
+		return list;
+	}
+
+	public void setList(List<GoodsVO> list) {
+		this.list = list;
+	}
 }
