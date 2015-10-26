615af15a17cf5131384bb2be4f873532cbc04015
Panda96 <878924390@qq.com>
2015-10-26 23:19:33 +0800

    漏了一个

--- ExpressSystemGAP/src/client/vo/StockCheckVO.java
+++ ExpressSystemGAP/src/client/vo/StockCheckVO.java
@@ -1,5 +1,17 @@
 package client.vo;
 
-public class StockCheckVO {
+import java.util.List;
 
+public class StockCheckVO {
+	private List<GoodsVO> list;
+
+	public List<GoodsVO> getList() {
+		return list;
+	}
+
+	public void setList(List<GoodsVO> list) {
+		this.list = list;
+	}
+	
+	
 }
