bdf5b624782fc7dde3aa0892241a66425b8db11c
Christine-Seven <txy24601@126.com>
2015-10-26 20:13:43 +0800

    VO

--- ExpressSystemGAP/src/client/vo/CityVO.java
+++ ExpressSystemGAP/src/client/vo/CityVO.java
@@ -1,5 +1,18 @@
 package client.vo;
 
-public class CityVO {
+import java.util.List;
 
+import po.InstitutionPO;
+
+public class CityVO {
+	String cityName;
+	
+	public CityVO(String cityName){
+		this.cityName=cityName;
+	}
+	
+	public List<InstitutionPO> getInstitution(){
+		InstitutionPO ins= new InstitutionPO();
+		return ins.getAll();
+	}
 }
