92f2e3df8b2add7f0c3b03d8656a289a3d931689
彬 申 <705103197@qq.com>
2015-11-15 14:47:05 +0800

    一些改动

--- ExpressManageSystemGAP/client/src/main/java/gap/client/ui/mainui/MainFrame.java
+++ ExpressManageSystemGAP/client/src/main/java/gap/client/ui/mainui/MainFrame.java
@@ -8,9 +8,15 @@ import gap.client.ui.util.SwingConsole;
 import java.awt.Cursor;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -23,13 +29,12 @@ public class MainFrame extends JFrame {
 	private JPanel pa;
 
 	public MainFrame() {
-		MainFrameListener listener = new MainFrameListener();
-		addMouseListener(listener);
-		addMouseMotionListener(listener);
+		
 		grid = new GridBagLayout();
 		gcons = new GridBagConstraints();
 		pa = (JPanel) getContentPane();
 		courierInitial();
+		
 	}
 
 	void courierInitial() {
@@ -61,6 +66,9 @@ public class MainFrame extends JFrame {
 
 		setUndecorated(true);
 		SwingConsole.run(this, 1000, 650);
+		MainFrameListener listener = new MainFrameListener(this);
+		addMouseListener(listener);
+		addMouseMotionListener(listener);
 	}
 
 	public void setMainPanel(final JPanel mainPanel) {
@@ -86,38 +94,88 @@ class MainFrameListener implements MouseListener, MouseMotionListener {
 	int ox, oy, frameWidth, frameHeight, frameX, frameY;
 	boolean titleselected, se_resizeselected, e_resizeselected,
 			s_resizeselected;
+	
+	Rectangle northEast = new Rectangle();
+	Rectangle east = new Rectangle();
+	Rectangle southEast = new Rectangle();
+	Rectangle south = new Rectangle();
+	Rectangle southWest = new Rectangle();
+	Rectangle west = new Rectangle();
+	Rectangle northWest = new Rectangle();
+	Rectangle north = new Rectangle();
+	Map<Rectangle, Cursor> cursorMap = new HashMap<>(8);
+	ArrayList<Rectangle> rectangles = new ArrayList<>(8);
+	
+	
+	public MainFrameListener(JFrame frame) {
+		// TODO Auto-generated constructor stub
+		this.frame = frame;
+		refrashLoaction();
+		rectangles.add(northEast);
+		rectangles.add(east);
+		rectangles.add(southEast);
+		rectangles.add(south);
+		rectangles.add(southWest);
+		rectangles.add(west);
+		rectangles.add(northWest);
+		rectangles.add(north);
+
+		
+		cursorMap.put(northEast,new Cursor(Cursor.NE_RESIZE_CURSOR) );
+		cursorMap.put(east,		new Cursor(Cursor.E_RESIZE_CURSOR) );
+		cursorMap.put(southEast,new Cursor(Cursor.NW_RESIZE_CURSOR) );
+		cursorMap.put(south,	new Cursor(Cursor.S_RESIZE_CURSOR) );
+		cursorMap.put(southWest,new Cursor(Cursor.SW_RESIZE_CURSOR) );
+		cursorMap.put(west,		new Cursor(Cursor.W_RESIZE_CURSOR) );
+		cursorMap.put(northWest,new Cursor(Cursor.NW_RESIZE_CURSOR) );
+		cursorMap.put(north,	new Cursor(Cursor.N_RESIZE_CURSOR) );
+		
+
+		
+		
+	}
+	
+	private void refrashLoaction(){
+		int width = frame.getWidth();
+		int height = frame.getHeight();
+		
+		northEast.setBounds(width-10, 0,10,10);
+		east.setBounds(width-10, 10, 10, height-20);
+		southEast.setBounds(width-10,height-10,10,10);
+		south.setBounds(10, height-10, width-20, 10);
+		southWest.setBounds(0,height-10,10,10);
+		west.setBounds(0, 10, 10, height-20);
+		northWest.setBounds(0, 0, 10, 10);
+		north.setBounds(10, 0, width-20, 10);		
+	}
+	
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
 		// TODO 自动生成的方法存根
-		frame = (JFrame) e.getSource();
-		int x = e.getX(), y = e.getY(), dx = x - ox, dy = y - oy;
-		if (titleselected)
-			frame.setLocation(frame.getX() + dx, frame.getY() + dy);
-		if (se_resizeselected)
-			frame.setSize(frameWidth + dx, frameHeight + dy);
-		if (e_resizeselected)
-			frame.setSize(frameWidth + dx, frameHeight);
-		if (s_resizeselected)
-			frame.setSize(frameWidth, frameHeight + dy);
 
+		for(Rectangle rect:cursorMap.keySet()){
+			if(rect.contains(e.getPoint())){
+				frame.setCursor(cursorMap.get(rect));
+			}
+		}
+
+		
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
 		// TODO 自动生成的方法存根
-		frame = (JFrame) e.getSource();
-		int x = e.getX(), y = e.getY(), width = frame.getWidth(), height = frame
-				.getHeight();
-		if ((width - x) <= 10 && (height - y) <= 10)
-			frame.setCursor(new Cursor(Cursor.SE_RESIZE_CURSOR));
-		else if ((width - x) <= 10 && (10 <= y && y <= height - 10))
-			frame.setCursor(new Cursor(Cursor.E_RESIZE_CURSOR));
-		else if ((height - y) <= 10 && 10 <= x && x <= width - 10)
-			frame.setCursor(new Cursor(Cursor.S_RESIZE_CURSOR));
-		else
-			frame.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
-
+		refrashLoaction();
+	
+		for(Rectangle rect:cursorMap.keySet()){
+			if(rect.contains(e.getPoint())){
+				frame.setCursor(cursorMap.get(rect));
+			}
+		}
+	
+		
+	
 	}
 
 	@Override
@@ -129,7 +187,7 @@ class MainFrameListener implements MouseListener, MouseMotionListener {
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO 自动生成的方法存根
-		frame = (JFrame) e.getSource();
+	
 		int width = frame.getWidth(), height = frame.getHeight();
 		frameWidth = width;
 		frameHeight = height;
--- ExpressManageSystemGAP/client/src/main/java/gap/client/ui/util/SwingConsole.java
+++ ExpressManageSystemGAP/client/src/main/java/gap/client/ui/util/SwingConsole.java
@@ -34,10 +34,10 @@ public class SwingConsole {
 	}
 
 	public static void run(final JFrame f, final int width, final int height) {
-		SwingUtilities.invokeLater(new Runnable() {
 
-			@Override
-			public void run() {
+
+
+
 				// TODO �Զ����ɵķ������
 				if (closeOperation)
 					f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -57,8 +57,8 @@ public class SwingConsole {
 				} else
 					f.setLocation(0, 0);
 
-			}
-		});
+		
+
 
 	}
 
