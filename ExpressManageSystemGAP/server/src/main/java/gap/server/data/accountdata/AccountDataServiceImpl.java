b4ff6fd97bdab15e75cf45f5655584cac44d073e
彬 申 <705103197@qq.com>
2015-11-22 21:18:06 +0800

    一点改动

--- ExpressManageSystemGAP/server/src/main/java/gap/server/data/accountdata/AccountDataServiceImpl.java
+++ ExpressManageSystemGAP/server/src/main/java/gap/server/data/accountdata/AccountDataServiceImpl.java
@@ -1,5 +1,62 @@
 package gap.server.data.accountdata;
 
-public class AccountDataServiceImpl {
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.jar.Attributes.Name;
+
+import com.mysql.jdbc.BalanceStrategy;
+
+import gap.common.dataservice.accountdataservice.AccountDataService;
+import gap.common.po.AccountPO;
+import gap.common.po.Cost_profitPO;
+import gap.common.po.TradePO;
+import gap.server.databaseutility.Excutor;
+
+public class AccountDataServiceImpl implements AccountDataService{
+
+	String name= "Name";
+	String balance = "Baleance";
+	String income="Income";
+	String pay="Pay";
+	Excutor excutor = Excutor.getInstance();
+	@Override
+	public boolean add(AccountPO accountPO) throws RemoteException {
+		// TODO Auto-generated method stub
+		String name = accountPO.getName();
+		StringBuilder builder = new StringBuilder("Insert into ");
+		
+		
+		return true;
+	}
+
+	@Override
+	public boolean delete(AccountPO accountPO) throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean modify(AccountPO accountPO) throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean trade(TradePO tradePO) throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public ArrayList<Cost_profitPO> getCost_Profit() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<AccountPO> getAccountList() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
---
+++ ExpressManageSystemGAP/server/src/main/java/gap/server/data/util/SQLBuilder.java
@@ -0,0 +1,114 @@
+package gap.server.data.util;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import gap.server.databaseutility.Excutor;
+
+/**
+ * 
+ * 一个SQL生成器，可以直接执行，用法示例如下:<p/>
+ * 
+ *	SQLBuilder_Shen sqlBuilder = new SQLBuilder_Shen();	<br/>
+ *	sqlBuilder.Select("Name","Balance","Income").From("account").Where("Balance > 10000");<br/>
+ *	sqlBuilder.excuteQuery();
+ *		
+ * @author 申彬
+ *
+ */
+public class SQLBuilder {
+	
+	public static void main(String[] args) {
+		SQLBuilder sqlBuilder = new SQLBuilder();		
+		sqlBuilder.Select("Name","Balance","Income").From("account").Where("Balance > 10000");
+		sqlBuilder.excuteQuery();
+		System.out.println(sqlBuilder.getSQL());
+		
+	}
+
+	private Excutor excutor;
+	private StringBuilder builder;
+	public SQLBuilder(){
+		excutor = Excutor.getInstance();
+		builder = new StringBuilder();
+	}
+	
+	public boolean excute(){
+		boolean result = false;
+		try {
+			result = excutor.excute(builder.toString());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("兄弟你别乱执行呀");
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	public ResultSet excuteQuery(){
+		ResultSet resultSet = null;
+		try {
+			resultSet = excutor.excuteQuery(builder.toString());
+		} catch (SQLException e) {
+			// TODO: handle exception
+			System.out.println("兄弟你别乱执行呀");
+			e.printStackTrace();
+		}
+		return resultSet;		
+	}
+	
+	public String getSQL(){
+		return builder.toString();
+	}
+	
+	public SQLBuilder Select(String ... column){
+		builder.append("select ");
+		builder.append(column[0]);
+		
+		for(int i=1;i<column.length;i++){
+			builder.append(",");
+			builder.append(column[i]);	
+		}	
+		builder.append(" ");
+		return this;
+	}
+	
+	public SQLBuilder From(String tableName){
+		builder.append("from ");
+		builder.append(tableName);
+		builder.append(" ");
+		return this;
+	}
+	
+	
+	/**
+	 * 
+	 * 若传入列名，记得调用 EQUALS，或者 AND，或者OR，EQUALS相当于 = 
+	 * 直接传入表达式也行
+	 * @param col_Or_express
+	 * @return
+	 */
+	public SQLBuilder Where(String col_Or_express){
+		builder.append("Where ");
+		builder.append(col_Or_express);
+		builder.append(" ");
+		return this;
+	}
+	
+//	public SQLBuilder_Shen EQUALS(int num){
+//		
+//	}
+	
+	private void judgeKeyWord(String judgeWord){
+		builder.append(" ");
+		builder.append(judgeWord);
+		builder.append(" ");
+	}
+	
+	public void clear(){
+		if(builder!=null){
+			builder.delete(0, builder.length());
+		}
+	}
+	
+}
