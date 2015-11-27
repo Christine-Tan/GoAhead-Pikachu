b4c3f92968cfdf67c02e1b92784a4ab5764b4f31
彬 申 <705103197@qq.com>
2015-11-27 23:53:36 +0800

    改

--- ExpressManageSystemGAP/client/src/main/java/gap/client/util/FuzzyQuery.java
+++ ExpressManageSystemGAP/client/src/main/java/gap/client/util/FuzzyQuery.java
@@ -15,7 +15,7 @@ public class FuzzyQuery {
 	 * 从keyword的第一个字符开始，从头开始遍历一个字符串，每找到一个对应字符，<br/>
 	 * 检查 连续匹配的boolean是否为true，是则加200，若不是，将其基础权值加到累加器<br/>
 	 * 	，并且设置连续位为true，keyword的index++，接着找。<p/>
-	 * 该算法会将连续出现keyword中连续字符的串排在前边，假如连续情况相同，短的排前边<br>
+	 * 该算法会将连续出现keyword中连续字符的串排在前边，假如连续情况相同，追加一个判断，短的排前边<br>
 	 * @param source
 	 * @param keyword
 	 * @return
