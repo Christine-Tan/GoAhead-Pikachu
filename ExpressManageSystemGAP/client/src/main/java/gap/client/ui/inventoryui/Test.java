ecfa67505c71decf58d47ad777529f7c9567c4fe
Panda96 <878924390@qq.com>
2015-12-01 21:22:51 +0800

    UI尝试

---
+++ ExpressManageSystemGAP/client/src/main/java/gap/client/ui/inventoryui/Test.java
@@ -0,0 +1,18 @@
+package gap.client.ui.inventoryui;
+
+import gap.client.ui.BaseComponents.MainFrame;
+import gap.client.ui.BaseComponents.UserBox.Sex;
+import gap.client.ui.UITools.ConstInfo;
+
+
+public class Test {
+	public static void main(String []args){
+		ConstInfo.setJob("库存管理人员");
+		ConstInfo.setName("王小二");
+		ConstInfo.setNumber("000000001");
+		ConstInfo.setSex(Sex.man);
+		MainFrame mainFrame=new MainFrame();
+		AlarmValueSetPanel panel=new AlarmValueSetPanel(mainFrame);
+		mainFrame.setMainPanel(panel);
+	}
+}
