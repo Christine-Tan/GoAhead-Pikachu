6b3c74e3b3c9836b2c0ed0d5b1247ecb4c8543e7
Christine-Seven <txy24601@126.com>
2015-12-06 19:51:54 +0800

    salary

--- ExpressManageSystemGAP/client/src/main/java/gap/client/ui/managerui/salaryui/TitlePanel.java
+++ ExpressManageSystemGAP/client/src/main/java/gap/client/ui/managerui/salaryui/TitlePanel.java
@@ -1,7 +1,5 @@
-package gap.client.ui.managerui.rentui;
+package gap.client.ui.managerui.salaryui;
 
-import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
@@ -11,32 +9,27 @@ import java.awt.Insets;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import gap.client.ui.UITools.Default;
 import gap.client.ui.UITools.RenderSetter;
 import gap.client.ui.UITools.SwingConsole;
 import gap.client.ui.gapcomponents.ComponentStyle;
 import gap.client.ui.gapcomponents.GAPLabel;
 
 public class TitlePanel extends JPanel {
-	JLabel institution, money;
+	JLabel usertype, money;
 	GridBagLayout gb;
 	GridBagConstraints gcons;
 
 	public TitlePanel() {
-		setPreferredSize(new Dimension(Default.PANEL_WIDTH, 80));
-		setBackground(Color.WHITE);
-		institution = new GAPLabel("机构名称");
-		money = new GAPLabel("租金");
-        
-		//布局
-		gb=new GridBagLayout();
-		gcons=new GridBagConstraints();
+		usertype = new GAPLabel("人员类型");
+		money = new GAPLabel("薪水价格");
+		// 布局
+		gb = new GridBagLayout();
+		gcons = new GridBagConstraints();
 		setLayout(gb);
-		gcons.insets=new Insets(40,50,10,0);
-		SwingConsole.addComponent(gb, gcons, this, institution, 0,0, 1, 1, 0, 0);
-		gcons.insets=new Insets(40,130,10,450);
+		gcons.insets = new Insets(40, 50, 10, 0);
+		SwingConsole.addComponent(gb, gcons, this, usertype, 0, 0, 1, 1, 0, 0);
+		gcons.insets = new Insets(40, 130, 10, 450);
 		SwingConsole.addComponent(gb, gcons, this, money, 1, 0, 1, 1, 0, 0);
-		
 	}
 	
 	public void paintComponent(Graphics g){
