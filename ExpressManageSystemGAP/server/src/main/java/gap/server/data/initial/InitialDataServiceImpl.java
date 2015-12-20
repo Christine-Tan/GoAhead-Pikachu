cb87abbaeefe188e5ee5a08b61c3a518aca148dc
彬 申 <705103197@qq.com>
2015-12-20 22:38:17 +0800

    改

--- ExpressManageSystemGAP/server/src/main/java/gap/server/data/initial/InitialDataServiceImpl.java
+++ ExpressManageSystemGAP/server/src/main/java/gap/server/data/initial/InitialDataServiceImpl.java
@@ -1,5 +1,23 @@
 package gap.server.data.initial;
 
-public class InitialDataServiceImpl {
+import java.rmi.RemoteException;
+import java.util.List;
+
+import gap.common.dataservice.initialdata.InitialDataService;
+import gap.common.po.InitialPO;
+
+public class InitialDataServiceImpl implements InitialDataService{
+
+	@Override
+	public boolean addInitial(InitialPO initialPO) throws RemoteException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<InitialPO> getHistory() throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
