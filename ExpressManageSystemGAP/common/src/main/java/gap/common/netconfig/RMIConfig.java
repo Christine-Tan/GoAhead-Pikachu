3e661d71710a6dd35728d8023157276b22bd31ad
Christine-Seven <txy24601@126.com>
2015-12-29 20:19:06 +0800

    机构

--- ExpressManageSystemGAP/common/src/main/java/gap/common/netconfig/RMIConfig.java
+++ ExpressManageSystemGAP/common/src/main/java/gap/common/netconfig/RMIConfig.java
@@ -2,5 +2,5 @@ package gap.common.netconfig;
 
 public class RMIConfig {
 	public final static int RMI_port = 6600;
-	public final static String url = "rmi://127.0.0.1:" + RMI_port + "/";
+	public final static String url = "rmi://114.212.43.201:" + RMI_port + "/";
 }
