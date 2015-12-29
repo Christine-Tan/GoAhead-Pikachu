60fec77ef8e3246b62c2fae0727117d006b794a8
彬 申 <705103197@qq.com>
2015-12-29 23:31:22 +0800

    gai

--- ExpressManageSystemGAP/common/src/main/java/gap/common/netconfig/RMIConfig.java
+++ ExpressManageSystemGAP/common/src/main/java/gap/common/netconfig/RMIConfig.java
@@ -2,10 +2,7 @@ package gap.common.netconfig;
 
 public class RMIConfig {
 	public final static int RMI_port = 6600;
-<<<<<<< HEAD
-	public final static String url = "rmi://192.168.1.111:" + RMI_port + "/";
-=======
 //	114.212.43.201
 	public final static String url = "rmi://127.0.0.1:" + RMI_port + "/";
->>>>>>> origin/master
+
 }
