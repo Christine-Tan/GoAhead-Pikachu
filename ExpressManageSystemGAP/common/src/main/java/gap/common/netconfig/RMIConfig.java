3585f8e0491975e01f28e7e3f288b5a4c5ee0199
yyf013932 <835168932@qq.com>
2016-01-03 21:29:17 +0800

    yyf

--- ExpressManageSystemGAP/common/src/main/java/gap/common/netconfig/RMIConfig.java
+++ ExpressManageSystemGAP/common/src/main/java/gap/common/netconfig/RMIConfig.java
@@ -6,22 +6,25 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.ObjectInputStream.GetField;
 
 public class RMIConfig {
 	public static int RMI_port;
-	public static String url;// = "rmi://127.0.0.1:" + RMI_port + "/";
-	
-	static{
+	public static String url;
+	// = "rmi://127.0.0.1:" + RMI_port + "/";
+
+	static {
 		BufferedReader reader = null;
 		try {
-			reader = new BufferedReader(new FileReader(new File("../RMIConfig.txt")));
+			reader = new BufferedReader(new FileReader(
+					new File("RMIConfig.txt")));
 			String IP = reader.readLine();
 			RMI_port = Integer.parseInt(reader.readLine());
-			url = "rmi://"+IP+":"+RMI_port+"/";
+			url = "rmi://" + IP + ":" + RMI_port + "/";
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally {
+		} finally {
 			try {
 				reader.close();
 			} catch (IOException e) {
