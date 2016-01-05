9de52cd7b5f8f7739faedc579c36cae961bc651f
Christine-Seven <txy24601@126.com>
2016-01-05 10:32:04 +0800

    ‘

--- ExpressManageSystemGAP/client/src/main/java/gap/client/ui/BaseComponents/FrameInitialler/ManagerInitialler.java
+++ ExpressManageSystemGAP/client/src/main/java/gap/client/ui/BaseComponents/FrameInitialler/ManagerInitialler.java
@@ -11,24 +11,26 @@ import gap.client.ui.managerui.priceui.PricePanel;
 import gap.client.ui.managerui.rentui.RentPanel;
 import gap.client.ui.managerui.salaryui.SalaryPanel;
 import gap.client.ui.tableUI.Cost_profitUI.CostProfitPanel;
+import gap.client.ui.tableUI.OperationUI.OperationMainPanel;
 
-public class ManagerInitialler extends FrameInitialler{
+public class ManagerInitialler extends FrameInitialler {
 	/**
 	 * 初始化总经理界面
 	 * 
 	 * @param mainFrame
 	 */
-   
-	protected void specificInitial(MainFrame mainFrame){
-		NavigateBar navBar=mainFrame.getNavigateBar();
-		NavigateButton approval=new NavigateButton(mainFrame, new ApprovalPanel(mainFrame), navBar, "审批单据");
-		NavigateButton institution=new NavigateButton(mainFrame, new InstitutionPanel(mainFrame), navBar, "机构管理");
-		NavigateButton rent=new NavigateButton(mainFrame, new RentPanel(mainFrame), navBar, "制定租金");
-		NavigateButton price=new NavigateButton(mainFrame, new PricePanel(mainFrame), navBar, "制定价格常量");
-		NavigateButton salary=new NavigateButton(mainFrame, new SalaryPanel(mainFrame), navBar, "制定薪水策略");
-		NavigateButton account=new NavigateButton(mainFrame, new AccountManagePanel(mainFrame), navBar, "查看银行账户");
-		NavigateButton table=new NavigateButton(mainFrame, new CostProfitPanel(mainFrame), navBar, "查看成本收益表");
-		NavigateButton log=new NavigateButton(mainFrame, new LogPanel(mainFrame), navBar, "查看日志记录");
+
+	protected void specificInitial(MainFrame mainFrame) {
+		NavigateBar navBar = mainFrame.getNavigateBar();
+		NavigateButton approval = new NavigateButton(mainFrame, new ApprovalPanel(mainFrame), navBar, "审批单据");
+		NavigateButton institution = new NavigateButton(mainFrame, new InstitutionPanel(mainFrame), navBar, "机构管理");
+		NavigateButton rent = new NavigateButton(mainFrame, new RentPanel(mainFrame), navBar, "制定租金");
+		NavigateButton price = new NavigateButton(mainFrame, new PricePanel(mainFrame), navBar, "制定价格常量");
+		NavigateButton salary = new NavigateButton(mainFrame, new SalaryPanel(mainFrame), navBar, "制定薪水策略");
+		NavigateButton account = new NavigateButton(mainFrame, new AccountManagePanel(mainFrame), navBar, "查看银行账户");
+		NavigateButton table = new NavigateButton(mainFrame, new CostProfitPanel(mainFrame), navBar, "查看成本收益表");
+		NavigateButton opeCondition = new NavigateButton(mainFrame, new OperationMainPanel(mainFrame), navBar, "查看经营状况表");
+		NavigateButton log = new NavigateButton(mainFrame, new LogPanel(mainFrame), navBar, "查看日志记录");
 		navBar.setHeight(50);
 		navBar.addButton(approval);
 		navBar.addButton(institution);
@@ -37,6 +39,7 @@ public class ManagerInitialler extends FrameInitialler{
 		navBar.addButton(salary);
 		navBar.addButton(account);
 		navBar.addButton(table);
+		navBar.addButton(opeCondition);
 		navBar.addButton(log);
 		navBar.setSelect(approval);
 		mainFrame.setVisible(true);
